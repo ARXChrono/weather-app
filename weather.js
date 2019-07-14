e8c2fc3f6ed735e9b10349c3ef5e50014a3bbb29
ARXChrono <ChronoXP@hotmail.com>
2019-07-14 17:43:01 +0800

    catch and log errors

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
+const http = require('http')
 const https = require('https')
 const querystring = require('querystring')
-const api = require('./api')
+const api = require('./api.json')
 
+// Print out temp details
 function printWeather(weather) {
   const message = `Current temperature in ${weather.name} is ${
     weather.main.temp
@@ -9,33 +11,61 @@ function printWeather(weather) {
   console.log(message)
 }
 
+// Print out error message
+function printError(error) {
+  console.error(error.message)
+}
+
 function get(query) {
-  const parameters = {
-    APPID: api.key,
-    units: 'metric',
-  }
+  try {
+    const parameters = {
+      APPID: api.key,
+      units: 'metric',
+    }
 
-  const zipCode = parseInt(query)
-  if (!isNaN(zipCode)) {
-    parameters.zip = zipCode + ',au'
-  } else {
-    parameters.q = query + ',au'
-  }
+    const zipCode = parseInt(query)
+    if (!isNaN(zipCode)) {
+      parameters.zip = zipCode + ',au'
+    } else {
+      parameters.q = query + ',au'
+    }
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(
-    parameters
-  )}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(
+      parameters
+    )}`
+    console.log(url)
 
-  const request = https.get(url, response => {
-    let body = ''
-    response.on('data', chunk => {
-      body += chunk
+    const request = https.get(url, response => {
+      if (response.statusCode === 200) {
+        let body = ''
+        // Read the data
+        response.on('data', chunk => {
+          body += chunk
+        })
+        response.on('end', () => {
+          try {
+            //Parse data
+            const weather = JSON.parse(body)
+            //Print the data
+            printWeather(weather)
+          } catch (error) {
+            //Parser error
+            printError(error)
+          }
+        })
+      } else {
+        // Status error code
+        const statusErrorCode = new Error(
+          `There was an error getting the message for "${query}". (${
+            http.STATUS_CODES[response.statusCode]
+          })`
+        )
+        printError(statusErrorCode)
+      }
     })
-    response.on('end', () => {
-      const weather = JSON.parse(body)
-      printWeather(weather)
-    })
-  })
+  } catch (error) {
+    printError(error)
+  }
 }
 
 module.exports.get = get
